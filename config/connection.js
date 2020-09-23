6a8ad66676c05325c67b9adf0269ac7c1c1e0740
Brandon Ketterman <brandonkett24@gmail.com>
2020-09-23 19:43:42 -0400

    added schema and controller

--- config/connection.js
+++ config/connection.js
@@ -1,13 +1,17 @@
+// Connect Node to MySQL.
 var mysql = require("mysql");
-require("dotenv").config();
 
-var connection = mysql.createConnection({
-  host: "localhost",
-  port: 3306,
-  user: "root",
-  password: process.env.mySqlPW,
-  database: "pets_db",
-});
+if (process.env.JAWSDB_URL) {
+  connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+  connection = mysql.createConnection({
+    host: "localhost",
+    user: "root",
+    password: "Colton24!",
+    database: "burgers_db",
+  });
+}
+
 connection.connect(function (err) {
   if (err) {
     console.error("error connecting: " + err.stack);
@@ -15,4 +19,6 @@ connection.connect(function (err) {
   }
   console.log("connected as id " + connection.threadId);
 });
+
+// Export the connection.
 module.exports = connection;
