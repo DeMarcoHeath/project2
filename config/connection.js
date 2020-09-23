e67e0128cab553b1071ce99b8483d10967ad95d7
Arob7 <66214652+Arob7@users.noreply.github.com>
2020-09-23 16:45:53 -0400

    added .gitignore, packages and dotenv

--- config/connection.js
+++ config/connection.js
@@ -1,12 +1,14 @@
 var mysql = require("mysql");
+require("dotenv").config();
+
 var connection = mysql.createConnection({
   host: "localhost",
   port: 3306,
   user: "root",
-  password: "",
-  database: "pets_db"
+  password: process.env.mySqlPW,
+  database: "pets_db",
 });
-connection.connect(function(err) {
+connection.connect(function (err) {
   if (err) {
     console.error("error connecting: " + err.stack);
     return;
