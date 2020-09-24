b10cb41d920cb08684f13ebf9e411dfb329df647
Demarco Heath <demarcoheath@gmail.com>
2020-09-23 21:10:43 -0400

    update database

--- db/schema.sql
+++ db/schema.sql
@@ -1,12 +1,12 @@
--- Create the database burgers_db.
+-- Create the database rentals_db.
 CREATE DATABASE rentals;
 USE rentals_db;
 
--- Create the table burgers.
+-- Create the table bikes.
 CREATE TABLE bikes
 (
     id INT NOT NULL AUTO_INCREMENT,
-    burger_name VARCHAR(50) NOT NULL,
+    bike_name VARCHAR(50) NOT NULL,
     devoured BOOLEAN DEFAULT false,
     PRIMARY KEY (id)
 );
